eeb122f89e925a3ab4c5e6b5639ec374a463f625
deniscozma2019 <denis.cozma2015@gmail.com>
2023-03-27 12:29:39 +0300

    Initial commit

---
+++ src/main/java/com/example/Lab3/JavaEE/JavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab3.JavaEE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEeApplication.class, args);
+	}
+
+}
